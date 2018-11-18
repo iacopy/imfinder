f504b9cc12e5efeb1f7499d08ee807c5923a3075
iacopy <iacopomarmorini@gmail.com>
2018-11-18 16:34:48 +0100

    imdispatcher: multiple template dirs
    you can have multiple dirs with multiple templates
    for each dir

--- imdispatcher.py
+++ imdispatcher.py
@@ -1,4 +1,25 @@
 """
+dispatch:
+
+Create N templates directory where to put N kind of images.
+Each template directory is allowed to contain several template images
+sice the same kind of image could have different presentation
+(e.g. app changing layout or gui or pages)
+
+root/
+    pippo1.jpg
+    pippo2.jpg
+    pluto1.jog
+    pluto2.jpg
+    template_pippo/
+        template__pippo.png
+    template_pluto/
+        template__pluto_a.png
+        template__pluto_b.png
+
+
+dispatch_legacy:
+
 Create a markers directory where to put N template images for N targets.
 You can have only 1 template for 1 target.
 
@@ -12,6 +33,7 @@ root/
         template_pluto.jpg
 """
 import os
+import shutil
 import sys
 
 import cv2
@@ -22,11 +44,12 @@ from imfinder import bool_find
 class Marker:
     def __init__(self, name, image_filepath):
         self.name = name
+        self.dst = name
         self.image = cv2.imread(image_filepath, 0)
         assert self.image is not None
 
     def __repr__(self):
-        return 'Marker({})'.format(self.name)
+        return 'Marker({})'.format(self.name)       
 
 
 def get_markers_images(dirpath, markers_dirname):
@@ -40,14 +63,42 @@ def get_markers_images(dirpath, markers_dirname):
             ret.append(marker)
     return ret
 
-def decide(image_filepath, markers):
+
+class Template:
+    def __init__(self, image_filepath):
+        dirpath, filename = os.path.split(image_filepath)
+        name = os.path.splitext(filename)[0]
+        self.name = name
+        self.dst = dirpath
+        self.image = cv2.imread(image_filepath, 0)
+        assert self.image is not None
+
+    def __repr__(self):
+        return 'Template({}[{}])'.format(self.name, self.image.shape)
+
+
+def get_templates_dirs(dirpath, templates_dirs):
+    ret = []
+    for dirname in templates_dirs:
+        templates_dirpath = os.path.join(dirpath, dirname)
+        for filename in os.listdir(templates_dirpath):
+            if filename.startswith('.') or not filename.startswith('template__'):
+                continue
+
+            template_image_path = os.path.join(templates_dirpath, filename)
+            template = Template(template_image_path)
+            ret.append(template)
+    return ret
+
+
+def decide(image_filepath, templates):
     image_gray = cv2.imread(image_filepath, 0)
     img_rgb = cv2.imread(image_filepath)
     # image_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    for marker in markers:
+    for template in templates:
         try:
-            if bool_find(marker.image, image_gray, threshold=0.9):
-                return marker.name
+            if bool_find(template.image, image_gray, threshold=0.95):
+                return template.dst
         except cv2.error as err:
             assert "_img.size().height <= _templ.size().height && _img.size().width <= _templ.size().width in function 'matchTemplate'" in str(err)
 
@@ -76,10 +127,37 @@ def dispatch_legacy(dirpath, markers_dirname='templates'):
     return ret
 
 
-def main(dirpath, markers_dirname='templates'):
-    # TODO: actual file dispatching
-    pass
+def dispatch(dirpath, templates_dirs, action=None):
+    ret = {}
+    templates = get_templates_dirs(dirpath, templates_dirs)
+    print(templates)
+    filenames = [filename for filename in os.listdir(dirpath) if not filename.startswith('.') \
+        and os.path.isfile(os.path.join(dirpath, filename))]
+    filenames.sort()
+    n_filenames = len(filenames)
+    for i, filename in enumerate(filenames):
+        image_filepath = os.path.join(dirpath, filename)
+
+        print('{:,}/{:,} ({:.01%}) {}'.format(i, n_filenames, i / n_filenames, filename))
+        dst = decide(image_filepath, templates)
+        if not dst:
+            continue
+
+        print(filename, '->', dst)
+        ret[filename] = dst
+
+        if action:
+            try:
+                action(image_filepath, dst)
+            except Exception as err:
+                print(err)
+    return ret
+
+
+def main(dirpath, templates_dirs):
+    print('main({}, {}'.format(dirpath, templates_dirs))
+    dispatch(dirpath, templates_dirs, action=shutil.move)
 
 
 if __name__ == '__main__':
-    dispatch(sys.argv[1])
+    main(sys.argv[1], sys.argv[2:])
--- test_imdispatcher.py
+++ test_imdispatcher.py
@@ -1,4 +1,4 @@
-from imdispatcher import dispatch_legacy
+from imdispatcher import dispatch, dispatch_legacy
 
 
 ROOT = 'test'
@@ -11,3 +11,14 @@ def test_imdispatcher_legacy():
     assert ret['road_alert_rotated.jpg'] == 'template_alert_rotated'
     assert ret['road_alert.png'] == 'template_alert'
     assert len(ret) == 3
+
+
+def test_imdispatcher():
+    ret = dispatch(ROOT, ['template_lena', 'template_health', 'template_alert', 'template_gui'])
+
+    assert ret['lena.jpg'] == 'test/template_lena'
+    assert ret['road_alert.png'] == 'test/template_alert'
+    assert ret['gui.png'] == 'test/template_gui'
+    assert ret['Health_1.png'] == 'test/template_health'
+    assert ret['Health_2.png'] == 'test/template_health'
+    assert len(ret) == 5
